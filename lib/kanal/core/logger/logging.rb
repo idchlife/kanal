483208bfc0c620f55f0b2cf3455fe79c81f42072
hulimax <maxt@pie.studio>
2023-03-07 12:46:44 +0400

    fix: unnsecessary method call (that broke calling class name) removed

--- lib/kanal/core/logger/logging.rb
+++ lib/kanal/core/logger/logging.rb
@@ -10,14 +10,10 @@ module Kanal
 
       # Mixing in Logger in some class adds possibility to use logger instance method
       def logger
-        @logger_instance = Logging.get_or_create_logger self.class.name
+        @logger_instance ||= Logging.create_logger self.class.name
       end
 
       class << self
-        def get_or_create_logger(class_name)
-          @logger_instance ||= create_logger(class_name)
-        end
-
         def create_logger(class_name)
           logger = Logger.new STDOUT
           logger.progname = class_name.rpartition(':').last
